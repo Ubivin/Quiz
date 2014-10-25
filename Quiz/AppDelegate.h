eb824851aa1aee034f3ccff84197f417b8d632f3
Ubivin <ubivin@gmail.com>
2014-10-25 16:04:37 -0700

    Initial Commit

---
+++ Quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by Jason Hoang on 10/25/14.
+//  Copyright (c) 2014 Jason Hoang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
